acad3d71a3364854340b3196167602191bdef13d
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-04 01:42:42 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,12 +17,12 @@ def consolidate_cart(cart)
   cart.dup.each do |item_hash|
     item_hash.each do
 
-      if find_item_by_name_in_collection(item_hash[:item], consolidated_cart) == nil
+      if find_item_by_name_in_collection(item_hash[:item], consolidated_cart.dup) == nil
         item_hash[:count] = 1
 
         consolidated_cart << item_hash
 
-      elsif find_item_by_name_in_collection(item_hash[:item], consolidated_cart) == true
+      elsif find_item_by_name_in_collection(item_hash[:item], consolidated_cart.dup) == true
         consolidated_cart[item_hash][:count] += 1
         
       end  
