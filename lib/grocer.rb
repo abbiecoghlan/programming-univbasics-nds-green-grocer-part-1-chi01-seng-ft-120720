2f6f79db4afabbef09cab17a39b1654f0463466d
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-04 01:31:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -1,10 +1,6 @@
 require "pry"
 
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
-  #
-  # Consult README for inputs and outputs
-
   collection.each do |item_hash|
     if item_hash[:item] == name
       return item_hash
@@ -19,6 +15,29 @@ def consolidate_cart(cart)
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
+  #iterate through iterate through each item in the unconsolidated cart 
+    # if it is not in the consolidated cart, add it to the consolidated cart 
+    # if it 
+
+  consolidated_cart = []
+  
+
+  
+  cart.each do |item_hash|
+    item_hash.each do
+
+      if find_item_by_name_in_collection(item_hash[:item], consolidated_cart) == nil
+        item_hash[:count] = 1
+
+        consolidated_cart << item_hash
+
+      elsif find_item_by_name_in_collection(item_hash[:item], consolidated_cart) == true
+        consolidated_cart[item_hash][:count] += 1
+        
+      end 
+    end
+  end    
+      
 
 end
 
