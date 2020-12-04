1ccac2c2091655d38f4e5c8ccbeea74e1ae0492e
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-04 02:49:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -17,22 +17,18 @@ def consolidate_cart(cart)
   
   cart.each do |item_hash|
     item_hash.dup.each do
-      if consolidated_cart.include? item_hash == false
+
+      if consolidated_cart.include? item_hash == true
+        consolidated_cart[item_hash][:count] += 1
+
+      else consolidated_cart.include? item_hash == false
         item_hash[:count] = 1
 
         consolidated_cart << item_hash
      
      end
 
-      if consolidated_cart.include? item_hash == true
-        binding.pry
-          consolidated_cart.each do |updated_item_hash|
-            if updated_item_hash == item_hash
-                updated_item_hash[:count] += 1
-            end
-          end
-        
-      end  
+
   
     end
   end    
