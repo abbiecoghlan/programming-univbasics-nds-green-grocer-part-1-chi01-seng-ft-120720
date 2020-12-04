83c051dfe5d1ac57e87ce54bd894a89c2e65cd69
Abbie Coghlan <abbie.coghlan@gmail.com>
2020-12-04 02:37:22 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -26,8 +26,10 @@ def consolidate_cart(cart)
 
       elsif find_item_by_name_in_collection(item_hash[:item], consolidated_cart) == !nil
           consolidated_cart.each do |updated_item_hash|
-            updated_item_hash[:count] += 1
-       
+            if updated_item_hash == item_hash
+                updated_item_hash[:count] += 1
+            end
+          end
         
       end  
     end
